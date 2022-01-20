48a02cc052fb46f8cbf266bc6a62676eb336e21e
AlinaPash <90476708+AlinaPash@users.noreply.github.com>
2022-01-20 21:15:30 +0300

    Update task7_func.cpp

--- 2lRBPO/2lRBPO/task7_func.cpp
+++ 2lRBPO/2lRBPO/task7_func.cpp
@@ -1,8 +1,8 @@
-/*#include <iostream>
+#include <iostream>
 #include <math.h>
 static double x = 7;
 static double result;
 static void f()
 {
 	result = (5 - 2 * x * x) / ((1 + x + x * x) / (2 * x + x * x) + 2 - (1 - x - x * x) / (2 * x - x * x));
-}*/
+}
